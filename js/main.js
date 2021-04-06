7d41adf3882685dfcc019e10a90c241eba3b15a9
newtsarthur <76273537+newtsarthur@users.noreply.github.com>
2021-04-06 04:43:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.sliderclass').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2200
+  });
